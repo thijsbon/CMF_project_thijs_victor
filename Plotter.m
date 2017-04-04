3ed95d320432df6623db1cd5cd5ad632d92bbe9b
thijsbon <thijsbon@outlook.com>
2017-04-04 12:17:39 +0100

    plot with legend and axis

--- Plotter.m
+++ Plotter.m
@@ -7,10 +7,13 @@ grid on
 u_analytic = -6*Q/H^3*(zc.^2-H*zc)*(prescribeswitch == 1)*(bcswitch == 0)*(uwall1 == 0) + ...
              1./(2.*mu).*dpdx.*(zc.^2-H*zc)*(prescribeswitch == 0)*(bcswitch == 0)*(uwall1 == 0); %poiseuille flow, i.e. v = 0 at walls, if Q is specified
 if show_analytic==1
-plot(u_analytic(2:end-1),zc(2:end-1),'r')
+plot(u_analytic(2:end-1),zc(2:end-1),'r'); legend('numerical','analytic');
+xlabel('u(m/s)'); ylabel('z(m)');
 end
 figure(2)
 hold on
 plot(u(2:end-1)/max(u),zc(2:end-1),'ob')
 plot(u_analytic(2:end-1)/max(u_analytic),zc(2:end-1),'r')
+legend('numerical','analytic')
+xlabel('u/u_max'); ylabel('z(m)')
 grid on
